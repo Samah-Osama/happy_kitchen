45046c3fec758e9460fa9a76953ee78a2f6e6d22
Samah <Samahosama112@icloud.com>
2024-09-16 23:58:05 +0300

    trigger all recipes cubit

--- lib/features/home/presentation/views/widgets/recipe_list.dart
+++ lib/features/home/presentation/views/widgets/recipe_list.dart
@@ -6,8 +6,21 @@ import 'package:happy_kitchen/core/widgets/custom_loading_indicator.dart';
 import 'package:happy_kitchen/features/home/presentation/view_model/all_recipe_cubit/all_recipes_cubit.dart';
 import 'package:happy_kitchen/features/home/presentation/views/widgets/recipe_widget.dart';
 
-class RecipesList extends StatelessWidget {
+class RecipesList extends StatefulWidget {
   const RecipesList({super.key});
+
+  @override
+  State<RecipesList> createState() => _RecipesListState();
+}
+
+class _RecipesListState extends State<RecipesList> {
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<AllRecipesCubit>(context).getAllRecipes();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<AllRecipesCubit, AllRecipesState>(
