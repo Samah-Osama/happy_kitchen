bdceb036d418f43d24830235ceb20437f97e025a
Samah <Samahosama112@icloud.com>
2024-09-18 00:00:54 +0300

    provide add recipe cubit to add recipe view

--- lib/features/add_recipe/presentation/views/add_recipe_view.dart
+++ lib/features/add_recipe/presentation/views/add_recipe_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:happy_kitchen/core/utils/theme_color_helper.dart';
+import 'package:happy_kitchen/features/add_recipe/presentation/view_model/add_recipe_cubit/add_recipe_cubit.dart';
 import 'package:happy_kitchen/features/add_recipe/presentation/views/widgets/add_recipe_view_body.dart';
 
 class AddRecipeView extends StatelessWidget {
@@ -7,13 +9,16 @@ class AddRecipeView extends StatelessWidget {
   static String id = 'AddRecipeView';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Add a Recipe"),
-        foregroundColor: ThemeColorHelper.getPrimaryColor(context),
+    return BlocProvider(
+      create: (context) => AddRecipeCubit(),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Add a Recipe"),
+          foregroundColor: ThemeColorHelper.getPrimaryColor(context),
+        ),
+        body: const AddRecipeViewBody(),
       ),
-      body: const AddRecipeViewBody(),
     );
   }
 }
