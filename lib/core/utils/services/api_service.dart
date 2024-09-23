894ff17aaa66439479e9d1fca95659c07f3fb058
Samah <Samahosama112@icloud.com>
2024-09-23 23:27:24 +0300

    creat KitchenTypeRepoImplementation

--- lib/core/utils/services/api_service.dart
+++ lib/core/utils/services/api_service.dart
@@ -5,6 +5,7 @@ class ApiService {
 
   ApiService({required this.dio});
   final String _baseUrl = 'http://88.223.94.70:8088/api/';
+                            
 
   Future<List<dynamic>> get({required String endpoint}) async {
     Response response = await dio.get('$_baseUrl$endpoint');
---
+++ lib/features/kitchens/data/repos/kitchen_type_repo_impel.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:happy_kitchen/core/errors/failure.dart';
+import 'package:happy_kitchen/core/utils/services/api_service.dart';
+import 'package:happy_kitchen/features/kitchens/data/models/kitchens_model/kitchens_model.dart';
+import 'package:happy_kitchen/features/kitchens/data/repos/kitchen_type_repo.dart';
+
+class KitchenTypeRepoImpel implements KitchenTypeRepo {
+  final ApiService apiService;
+
+  KitchenTypeRepoImpel({required this.apiService});
+  @override
+  Future<Either<Failure, List<KitchensModel>>>
+      fetchRecipeByKitchenType() async {
+    try {
+      var data = await apiService.get(endpoint: 'kitchen-types');
+      List<KitchensModel> kitchensList = [];
+      for (var kitchen in data) {
+        kitchensList.add(
+          KitchensModel.fromJson(kitchen),
+        );
+      }
+      return right(kitchensList);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      } else {
+        return left(
+          ServerFailure(
+            errorMessage: e.toString(),
+          ),
+        );
+      }
+    }
+  }
+}
